3729e519ab7d777990222a28cab64c596d4a8dc6
hidekuro <hidekuro@gmail.com>
2019-06-24 17:30:35 +0900

    improve installer
    - add aliases.local support
    - add Brewfile support

--- install.sh
+++ install.sh
@@ -1,4 +1,38 @@
+if [[ ! -d ~/.dotfiles ]]; then
+  git clone --recurse-submodules https://github.com/hidekuro/dotfiles.git ~/.dotfiles
+fi
+
+# vim
+mkdir -p ~/.vim/colors
+ln -snf ~/.dotfiles/iceberg.vim/colors/iceberg.vim ~/.vim/colors/iceberg.vim
+ln -snf ~/.dotfiles/.vimrc ~/.vimrc
+
+# bashrc
+ln -snf ~/.dotfiles/.bashrc ~/.bashrc
+touch ~/.bashrc.local
+
+# aliases
+if [[ -e ~/.aliases && ! -e ~/.aliases.local ]]; then
+  cat ~/.aliases >> ~/.aliases.local
+fi
+ln -snf ~/.dotfiles/.aliases ~/.aliases
+
+# git
+cp -n ~/.dotfiles/.gitconfig ~/.gitconfig
+cp -n ~/.dotfiles/.gitignore_global ~/.gitignore_global
+
+# brew
+if (type brew > /dev/null 2>&1); then
+  ln -snf ~/.dotfiles/Brewfile ~/Brewfile
+fi
+
+# zsh
 if (type zsh > /dev/null 2>&1); then
+  ln -snf ~/.dotfiles/.zshrc ~/.zshrc
+  touch ~/.zshenv
+  touch ~/.zshrc.local
+
+  # zsh-completions
   if [[ "${OSTYPE}" = darwin* ]]; then
     brew install zsh zsh-completions
   elif [[ "${OSTYPE}" = linux* ]]; then
@@ -7,25 +41,4 @@ if (type zsh > /dev/null 2>&1); then
     mkdir -p /usr/local/share
     git clone https://github.com/zsh-users/zsh-completions /usr/local/share/zsh-completions
   fi
-fi
-
-if [[ ! -d ~/.dotfiles ]]; then
-  git clone --recurse-submodules https://github.com/hidekuro/dotfiles.git ~/.dotfiles
-fi
-
-mkdir -p ~/.vim/colors
-ln -snf ~/.dotfiles/iceberg.vim/colors/iceberg.vim ~/.vim/colors/iceberg.vim
-ln -snf ~/.dotfiles/.vimrc ~/.vimrc
-ln -snf ~/.dotfiles/.aliases ~/.aliases
-ln -snf ~/.dotfiles/.bashrc ~/.bashrc
-touch ~/.bashrc.local
-touch ~/.aliases
-
-cp -n ~/.dotfiles/.gitconfig ~/.gitconfig
-cp -n ~/.dotfiles/.gitignore_global ~/.gitignore_global
-
-if (type zsh > /dev/null 2>&1); then
-  ln -snf ~/.dotfiles/.zshrc ~/.zshrc
-  touch ~/.zshenv
-  touch ~/.zshrc.local
 fi
